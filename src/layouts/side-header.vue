614ce8964c215b7ab946679d89d78c70a6137464
udo-bit <enote_gxl@163.com>
2024-08-22 00:12:20 +0800

    iframe

--- src/layouts/side-header.vue
+++ src/layouts/side-header.vue
@@ -4,7 +4,7 @@
 
 <template>
   <n-layout-header>
-    <div class="flex items-center h-full text-[#ed6e2d] font-700 text-2xl">
+    <div class="flex items-center h-full  text-[#ed6e2d] font-700 text-2xl">
       <slot/>
     </div>
   </n-layout-header>
